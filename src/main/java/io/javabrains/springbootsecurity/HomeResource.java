4ea7fa57ee86cc9760c828bb13479a92db70f611
jsf2184 <jsf2184@gmail.com>
2020-06-08 08:50:02 -0500

    initial commit

--- src/main/java/io/javabrains/springbootsecurity/HomeResource.java
+++ src/main/java/io/javabrains/springbootsecurity/HomeResource.java
@@ -1,5 +1,8 @@
 package io.javabrains.springbootsecurity;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,11 +16,27 @@ public class HomeResource {
 
     @GetMapping("/user")
     public String user() {
+        printPrincipal();
         return ("<h1>Welcome User</h1>");
+
     }
 
     @GetMapping("/admin")
     public String admin() {
+        printPrincipal();
         return ("<h1>Welcome Admin</h1>");
+    }
+
+    @GetMapping("/playful")
+    public String playful() {
+        return ("<h1>Welcome Playful</h1>");
+    }
+
+    public void printPrincipal() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Object principal = authentication.getPrincipal();
+        User user = (User) principal;
+        System.out.printf("principal = %s", user);
+
     }
 }
--- src/main/java/io/javabrains/springbootsecurity/SecurityConfiguration.java
+++ src/main/java/io/javabrains/springbootsecurity/SecurityConfiguration.java
@@ -2,18 +2,25 @@ package io.javabrains.springbootsecurity;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.provisioning.InMemoryUserDetailsManagerConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.*;
+import java.io.IOException;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-    @Override
+    //    @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         // Set your configuration on the auth object
-        auth.inMemoryAuthentication()
+        InMemoryUserDetailsManagerConfigurer<AuthenticationManagerBuilder> inMemoryUserDetailsManagerConfigurer = auth.inMemoryAuthentication();
+        inMemoryUserDetailsManagerConfigurer
                 .withUser("blah")
                 .password("blah")
                 .roles("USER")
@@ -25,15 +32,32 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
+        // no password encding -- clear text. Don't do this in a real application.
         return NoOpPasswordEncoder.getInstance();
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+
+        Filter filter = new Filter() {
+            @Override
+            public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+                filterChain.doFilter(servletRequest, servletResponse);
+            }
+        };
+
+        ExpressionUrlAuthorizationConfigurer<HttpSecurity>.ExpressionInterceptUrlRegistry urlRegistry = http.authorizeRequests();
+        urlRegistry
                 .antMatchers("/admin").hasRole("ADMIN")
                 .antMatchers("/user").hasAnyRole("ADMIN", "USER")
+                .antMatchers("/pla*").permitAll()
                 .antMatchers("/").permitAll()
-                .and().formLogin();
+                .and()
+                .formLogin()
+                .and()
+                .addFilterBefore(filter, BasicAuthenticationFilter.class);
+
+
+
     }
 }
